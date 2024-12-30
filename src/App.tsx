09e571d20841590ff9af3bf7d81da7597522e57b
Investgrape <alex@investgrape.com>
2024-12-29 22:01:40 -0800

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>RIA Dashboard</h1>
+      <p>Your dashboard will be here</p>
+    </div>
+  );
+};
+
+export default App;
